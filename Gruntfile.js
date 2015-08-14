7043bf1d281dee4961a7b2a07c7b9a12b501ed0d
Jason Yost <jyost@gocortexlabs.com>
2015-08-13 18:02:35 -0600

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		// Import package manifest
+		pkg: grunt.file.readJSON("package.json"),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.license %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			options: {
+				banner: "<%= meta.banner %>"
+			},
+			dist: {
+				src: ["src/jquery.checkground.js"],
+				dest: "dist/jquery.checkground.js"
+			}
+		},
+
+		// Lint definitions
+		jshint: {
+			files: ["src/jquery.checkground.js"],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			my_target: {
+				files: {
+        	"dist/jquery.checkground.min.js": ["src/jquery.checkground.js"],
+					"demo/javascripts/jquery.checkground.min.js": ["src/jquery.checkground.js"]
+      	}
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// CoffeeScript compilation
+		coffee: {
+			compile: {
+				files: {
+					"dist/jquery.checkground.js": "src/jquery.checkground.coffee"
+				}
+			}
+		},
+
+		jasmine: {
+	    test: {
+	      src: 'src/**/*.js'
+			},
+	      options: {
+	        specs: 'test/spec/test.js',
+	        keepRunner : false,
+					vendor: [
+        		'bower_components/jquery/dist/jquery.js',
+        		'bower_components/jasmine-jquery/lib/jasmine-jquery.js'
+      		],
+					outfile: 'test/_SpecRunner.html'
+	      }
+    },
+
+		// watch for changes to source
+		// Better than calling grunt a million times
+		// (call 'grunt watch')
+		watch: {
+		    files: ['src/*'],
+		    tasks: ['default']
+		}
+
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-coffee");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	grunt.registerTask("build", ["concat", "uglify"]);
+	grunt.registerTask("default", ["jshint", "jasmine", "build"]);
+	grunt.registerTask("travis", ["default"]);
+
+};
---
+++ src/jquery.checkground.js
@@ -0,0 +1,40 @@
+;(function ( $, window, document, undefined ) {
+
+	"use strict";
+
+		// Create the defaults once
+		var pluginName = "checkground",
+				defaults = {
+				propertyName: "value"
+		};
+
+		// The actual plugin constructor
+		function Plugin ( element, options ) {
+				this.element = element;
+				this.settings = $.extend( {}, defaults, options );
+				this._defaults = defaults;
+				this._name = pluginName;
+				this.init();
+		}
+
+		// Avoid Plugin.prototype conflicts
+		$.extend(Plugin.prototype, {
+				init: function () {
+						console.log("xD");
+				},
+				yourOtherFunction: function () {
+						// some logic
+				}
+		});
+
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function ( options ) {
+				return this.each(function() {
+						if ( !$.data( this, "plugin_" + pluginName ) ) {
+								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+						}
+				});
+		};
+
+})( jQuery, window, document );
