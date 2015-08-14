2de1f48758db2b267feaa25f352a15f574464c15
Jason Yost <jyost@gocortexlabs.com>
2015-08-13 20:40:38 -0600

    Basic plugin build

--- src/jquery.checkground.js
+++ src/jquery.checkground.js
@@ -1,40 +1,72 @@
-;(function ( $, window, document, undefined ) {
+;(function($, window, document, undefined) {
 
 	"use strict";
 
-		// Create the defaults once
-		var pluginName = "checkground",
-				defaults = {
-				propertyName: "value"
+	// Create the defaults once
+	var pluginName = "checkground",
+		defaults = {
+			debug: false
 		};
 
-		// The actual plugin constructor
-		function Plugin ( element, options ) {
-				this.element = element;
-				this.settings = $.extend( {}, defaults, options );
-				this._defaults = defaults;
-				this._name = pluginName;
-				this.init();
+	// The actual plugin constructor
+	function Plugin(element, options) {
+		this.element = element;
+		this.settings = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
+
+	// Avoid Plugin.prototype conflicts
+	$.extend(Plugin.prototype, {
+		init: function() {
+			var checkground = this;
+			checkground.initDom(checkground.element, checkground.settings);
+			checkground.bindElements(checkground.element, checkground.settings);
+			checkground.debugMessage(checkground.settings, "checkground initialized");
+		},
+		initDom: function(element, settings) {
+			var checkground = this;
+			$(element).hide();
+			$(element).addClass("checkground-original");
+			$(element).wrap("<div class=\"checkground-wrapper\"></div>");
+			var wrapper = $(element).parent();
+			if($(element).attr("checked")){
+				wrapper.addClass("checkground-checked");
+			}
+			checkground.debugMessage(settings, "dom initialized");
+		},
+		bindElements: function(element, settings){
+			var checkground = this;
+			var wrapper = $(element).parent();
+			wrapper.click(function(){
+				$(this).toggleClass("checkground-checked");
+				$(element).attr("checked", !$(element).attr("checked"));
+				checkground.debugMessage(settings, "checkground changed");
+			});
+			checkground.debugMessage(settings, "checkground bound on click");
+
+			$(element).change(function(){
+				wrapper.toggleClass("checkground-checked");
+				checkground.debugMessage(settings, "element changed");
+			});
+			checkground.debugMessage(settings, "element bound on change");
+		},
+		debugMessage: function(settings, message) {
+			if (this.settings.debug) {
+				console.log(message);
+			}
 		}
+	});
 
-		// Avoid Plugin.prototype conflicts
-		$.extend(Plugin.prototype, {
-				init: function () {
-						console.log("xD");
-				},
-				yourOtherFunction: function () {
-						// some logic
-				}
+	// A really lightweight plugin wrapper around the constructor,
+	// preventing against multiple instantiations
+	$.fn[pluginName] = function(options) {
+		return this.each(function() {
+			if (!$.data(this, "plugin_" + pluginName)) {
+				$.data(this, "plugin_" + pluginName, new Plugin(this, options));
+			}
 		});
+	};
 
-		// A really lightweight plugin wrapper around the constructor,
-		// preventing against multiple instantiations
-		$.fn[ pluginName ] = function ( options ) {
-				return this.each(function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-						}
-				});
-		};
-
-})( jQuery, window, document );
+})(jQuery, window, document);
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,79 @@
 (function () {
-  'use strict';
+  "use strict";
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-        expect(true).toBe(true);
+  describe("checkground()", function(){
+
+    jasmine.getFixtures().fixturesPath = "spec/fixtures";
+
+		var testInput, testWrapper;
+
+		beforeEach(function() {
+			loadFixtures("fixtures.html");
+			testInput = $(".default-test");
+			testInput.checkground();
+      testWrapper = testInput.parent();
+		});
+
+		afterEach(function(){
+			$("#test-container").remove();
+		});
+
+    describe("init", function(){
+
+      describe(".initDom()", function(){
+        it("should wrap the original control in div.checkground-wrapper", function(){
+          expect(testInput.parent()).toBeInDOM();
+          expect(testInput.parent()).toHaveClass("checkground-wrapper");
+        });
+
+        it("should hide the original control", function(){
+          expect(testInput).toBeHidden();
+        });
+
+        it("should include should add .checkground-original to the element", function(){
+          expect(testInput).toHaveClass("checkground-original");
+        });
+
+        describe("given a checked checkbox", function(){
+          it("should add the .checkground-checked on the wrapper element", function(){
+            testInput = $(".checked-test");
+            testInput.checkground();
+            testWrapper = testInput.parent();
+            expect(testWrapper).toHaveClass("checkground-checked");
+          });
+        });
+
       });
+
+      describe(".bindElements()", function(){
+        describe("div.checkground-wrapper on click", function(){
+          it("should toggle .checkground-checked on the wrapper element", function(){
+            testWrapper.click();
+            expect(testWrapper).toHaveClass("checkground-checked");
+
+            testWrapper.click();
+            expect(testWrapper).not.toHaveClass("checkground-checked");
+          });
+
+          it("should set the checked attribute of the original element", function(){
+            testWrapper.click();
+            expect(testInput).toHaveAttr("checked", "checked");
+
+            testWrapper.click();
+            expect(testInput).not.toHaveAttr("checked", "checked");
+          });
+        });
+
+        describe("input change", function(){
+          it("should toggle the checkground-checked class on change", function(){
+            testInput.attr("checked", true).trigger("change");
+            expect(testWrapper).toHaveClass("checkground-checked");
+          });
+        });
+      });
+
     });
+
   });
+
 })();
